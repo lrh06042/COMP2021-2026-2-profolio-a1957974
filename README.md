6b40000afa84e70237695af85ef51d312606b572
lrh06042 <lrh06042@163.com>
2026-08-11 02:09:10 +0930

    chore: add .gitignore and README

---
+++ README.md
@@ -0,0 +1,9 @@
+# COMP2021 Portfolio
+
+COMP2021 workshop and practical portfolio.
+
+## Week 1
+
+- Prac A
+  - IntroTopic
+  - PayrollCalculator
---
+++ Week1/PracA/IntroTopic/Program.cs
@@ -0,0 +1,5 @@
+﻿string message = "Hello, World!";
+int workshopNumber = 1;
+
+Console.WriteLine(message);
+Console.WriteLine($"Workshop {workshopNumber}");
